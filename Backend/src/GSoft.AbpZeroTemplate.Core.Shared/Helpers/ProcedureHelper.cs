58205b301ea3ff90e883cf90977973d54c87fc7a
baotg <16520088@gm.uit.edu.vn>
2020-06-18 00:53:27 +0700

    Search cuc manh =)))

--- Backend/src/GSoft.AbpZeroTemplate.Core.Shared/Helpers/ProcedureHelper.cs
+++ Backend/src/GSoft.AbpZeroTemplate.Core.Shared/Helpers/ProcedureHelper.cs
@@ -19,7 +19,7 @@ namespace GSoft.AbpZeroTemplate.Helpers
         private readonly string connectionString;
         public ProcedureHelper()
         {
-            connectionString = "Server=DESKTOP-1SV0JFT; Database=DbPratice; Trusted_Connection=True;";
+            connectionString = "Server=.; Database=DbPratice; Trusted_Connection=True;";
         }
 
         private List<ProcedureParamInfo> GetParamInfos(IDbConnection conn, string procedureName)
---
+++ Backend/src/modules/group2/Group2.AbpZeroTemplate.Application/Services/TaiXe/Dto/Group2TaiXeSearchInputDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group2.AbpZeroTemplate.Application.Share.Group2.Dto
+{
+    public class Group2TaiXeSearchInputDto
+    {
+        public int? TaiXe_Ma { get; set; }
+        public string TaiXe_HoTen { get; set; }
+        public string TaiXe_TrangThaiLamViec { get; set; }
+        public string TaiXe_HangBangLai { get; set; }
+        public DateTime? TaiXe_NgayBatDauLam { get; set; }
+        public long? TaiXe_MucLuongNho { get; set; }
+        public long? TaiXe_MucLuongLon { get; set; }
+    }
+}
